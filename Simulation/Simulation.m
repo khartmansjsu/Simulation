3a72c992d5f73c55956643cb9c79fdf37ef07394
khartman <32494415+khartmansjsu@users.noreply.github.com>
2020-07-29 16:11:19 -0700

    This is simulation patch 2.1
    This patch takes care of some sloppy programming in the initial commit. This branch addresses the following issues:
    https://github.com/khartmansjsu/MATLAB-Sim/issues/7
    https://github.com/khartmansjsu/MATLAB-Sim/issues/6
    https://github.com/khartmansjsu/MATLAB-Sim/issues/2

--- Simulation/Simulation.m
+++ Simulation/Simulation.m
@@ -1,11 +1,5 @@
 %% Simulation: This is the simulation first draft.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%% Version 2 adds thrusters for angular momentum %%%%%%%%%
-%%%%%%%%% and a function that can solve for position if %%%%%%%%%
-%%%%%%%%% a thruster is fired for some time.            %%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 clc
 clear all
 close all
@@ -17,30 +11,23 @@ global T
 %define simulation space frame
 Nframe = [1 0 0; 0 1 0; 0 0 1];
 
-T = 10;                      %duration of simulation
+T = 5;                      %duration of simulation
 dt = 0.01;                   %time increment
 t = 0;                       %initial time
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% STUFF THAT WILL HAPPEN IN THE GUI %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %define initial conditions
-phi_0 = 0;  %initial spin orientation (Bframe x-axis)
-psi_0 = 0;  %initial precession orientation
-theta_0 = 0;  % The newtation angle, it is between the spin axis and the angular momentum vector
-phi_dot = 0;  % initial rotation rate is zero (we will add rotation with a thruster)
-% If and as forces act on the object to rotate it in the precession and
-% newtation frames, the two below rates  will be defined and tracked as
-% properties of the object
-psi_dot = 0;
+phi_0 = 0;         % initial spin orientation (Bframe x-axis)
+psi_0 = 0;         % initial precession orientation
+theta_0 = 30;      % initial newtation angle, it is between the spin axis and the angular momentum vector
+phi_dot = 0;       % initial roll rate
+psi_dot = 0;  
 theta_dot = 0;
 %define object and load its initial conditions into its properties
 object1 = Cylinder;     %cylinder's mass properties are easy to define
 object1.mass = 5; object1.radius = 4; object1.height = 20; object1.inertial_tensor = object1.CalculateInertialTensor;
-object1.orientation = [phi_0; psi_0; theta_0];
-%NOTE: Thruster definitions needs to be developed more. They are super
-%buggy and don't work as intended.
-object1.Thruster1 = [0.4 0 2 0];    %define moment arm and thruster force
-object1.Thruster2 = [0.4 0 -2 0];    %define moment arm and thruster force
+object1.orientation = [psi_0; phi_0; theta_0];
 % define a cloud of points to track as I build this toolbox. Eventually
 % this point cloud will not be needed, and we will only need mass
 % properties and the locations of things like thrusters, pilots, fuel
@@ -52,28 +39,32 @@ N = 20;
 %this commit, but typically I inject it into the toolbox folder.
 object1.pointcloud = pointcloud(X,Y,Z*object1.height); object1.pointcloud(3,:) = object1.pointcloud(3,:) - object1.height/2;
 
+object1.Thruster1 = [0.2*[1;0;0] [0;4;0]];     %define Thruster force and thruster position
+object1.Thruster2 = [0.2*[-1;0;0] [0;4;0]];    %define Thruster force and thruster position
+
 % Rotate the currently upright cylinder to its defined initial orientation
-          R_na = [cosd(object1.orientation(2)) -sind(object1.orientation(2)) 0;        %Precession rotation matrix
-              sind(object1.orientation(2)) cosd(object1.orientation(2)) 0;
+          R_an = [cosd(object1.orientation(1)) sind(object1.orientation(1)) 0;        %Precession rotation matrix (reads: rotate to a from n)
+              -sind(object1.orientation(1)) cosd(object1.orientation(1)) 0;
               0 0 1];
-          R_ag = [1 0 0;                                                       %Newtation rotation matrix
-                  0 cosd(object1.orientation(3)) -sind(object1.orientation(3));     
-                  0 sind(object1.orientation(3)) cosd(object1.orientation(3))];
-          R_gb = [cosd(object1.orientation(1)) -sind(object1.orientation(1)) 0;        %Spin rotation matrix
-                  sind(object1.orientation(1)) cosd(object1.orientation(1)) 0;
+          R_ga = [1 0 0;                                                              %Newtation rotation matrix (reads: rotate to g from a)
+                  0 cosd(object1.orientation(3)) sind(object1.orientation(3));     
+                  0 -sind(object1.orientation(3)) cosd(object1.orientation(3))];
+          R_bg = [cosd(object1.orientation(2)) sind(object1.orientation(2)) 0;        %Spin rotation matrix (reads: rotate to b from g)
+                  -sind(object1.orientation(2)) cosd(object1.orientation(2)) 0;
                   0 0 1];
-           R_nb = R_na*R_ag*R_gb;                %Matrix from N-->B
-object1.position = R_nb*object1.pointcloud;
-disp(object1.position(:,N-1));    %displaying the initial tracked point of the point cloud for troubleshooting purposes
+          R_bn = R_bg*R_ga*R_an;                %  (rotates to b from n)
+object1.position = R_bn*object1.pointcloud;
+
+disp(object1.position(:,17));    %displaying the position of the two thrusters on the Cylinder
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% BEHIND THE SCENES %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Conversions of spin rates and entered into class phelp roperties
+%Conversions of spin rates and entered into class properties
 phi_dot = (180/pi)*phi_dot; %deg per second
 % psi_dot = 0; %rad/sec
 psi_dot = (180/pi)*psi_dot; %deg per sec
 theta_dot = (180/pi)*theta_dot; %deg per sec
-object1.spin_rates = [phi_dot; psi_dot; theta_dot];
+object1.spin_rates = [psi_dot; phi_dot; theta_dot];
 %% Loop
 %this for loop will eventually be "while Applied_Force or 
 %Applied_Moment == 0" and will live inside another for loop called "while sim = true"
@@ -85,8 +76,8 @@ tic
 while t <= T  %start at 2 for now, but eventually want it to be t_of_object_initialization + dt
       %%% ADD THE BELOW IN FOR A NICE MOVIE %%%
     plot3(Position(1,:,n),Position(2,:,n),Position(3,:,n),'b.'); axis equal; hold on;
-    plot3(Position(1,N-1,n),Position(2,N-1,n),Position(3,N-1,n),'r-o'); hold off
-    xlabel('x'); ylabel('y'); zlabel('z')
+    plot3(Position(1,17,n),Position(2,17,n),Position(3,17,n),'r-o'); hold off
+    xlabel('x'); ylabel('y'); zlabel('z');
     text(0,object1.height,sprintf('Timestamp: %f',t))
     title('Motion of the object')
     M(n) = getframe(gcf);
@@ -95,9 +86,9 @@ while t <= T  %start at 2 for now, but eventually want it to be t_of_object_init
     n = n + 1;
     if t < 3                                 %for three seconds, no forces are applied
         [object1.orientation, object1.position] = object1.freerotate(dt,Nframe);    
-    elseif (3 <= t) && (t <= 3.5)            %for a half a second, thruster 1 is fired
+    elseif (3 <= t) && (t <= 3.2)            %for a half a second, thruster 1 is fired
         [object1.spin_rates, object1.orientation, object1.position] = object1.firethruster(object1.Thruster1,dt,Nframe);    %here we fire a thruster, need to structure our method around this notation
-    elseif (7.5 <= t) && (t <= 8)            %for a half a second, thruster 2 is fired (opposite direction)
+    elseif (3.3 <= t) && (t <= 3.5)            %for a half a second, thruster 2 is fired (opposite direction)
         [object1.spin_rates, object1.orientation, object1.position] = object1.firethruster(object1.Thruster2,dt,Nframe);
     else                                     %for two seconds, no forces are applied
         [object1.orientation, object1.position] = object1.freerotate(dt,Nframe);
@@ -106,4 +97,4 @@ while t <= T  %start at 2 for now, but eventually want it to be t_of_object_init
 end
 toc
 disp('Final position of tracked point');
-disp(object1.position(:,N-1));
+disp(object1.position(:,17));
